197b97acd20aa2188d2013659862b8fee19c6ecc
plotiwitolp <plotiwitolp@gmail.com>
2022-12-20 09:26:16 +0400

    init

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Web-разработчик | Сайт-портфолио</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+
+<body>
+  <header>
+    <section>
+      <div class="header">
+        <div class="header__logo">
+          <svg class="icon">
+            <use xlink:href="./assets/sprite.svg#icon2"></use>
+          </svg>
+        </div>
+        <div class="header__nav">
+          <svg>
+
+          </svg>
+        </div>
+        <div class="header__social">
+
+        </div>
+      </div>
+    </section>
+  </header>
+  <main>
+    <section>
+      <input type="text" maxlength="2" id="sessionNo" name="sessionNum" />
+    </section>
+  </main>
+  <footer>
+    <section>
+
+    </section>
+  </footer>
+  <script src="./assets/js/script.js"></script>
+</body>
+
+</html>
